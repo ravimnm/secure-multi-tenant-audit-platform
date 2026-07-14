47442f2f4eae81c9eec9d53b5e53c746a3ebf597
Ravi Sankar Manem <manemravisankar28@gmail.com>
2026-07-14 11:56:19 +0530

    Update WebClientConfig.java

--- api-gateway/src/main/java/com/ivar/gateway/config/WebClientConfig.java
+++ api-gateway/src/main/java/com/ivar/gateway/config/WebClientConfig.java
@@ -3,6 +3,7 @@ package com.ivar.gateway.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.beans.factory.annotation.Value;;
 
 @Configuration
 public class WebClientConfig {
