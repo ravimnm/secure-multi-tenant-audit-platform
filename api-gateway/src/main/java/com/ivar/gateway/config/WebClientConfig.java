acf499ebff4871ac0147e2af9e4fcc4e4e33ba35
Ravi Sankar Manem <manemravisankar28@gmail.com>
2026-07-14 11:55:09 +0530

    Update WebClientConfig.java

--- api-gateway/src/main/java/com/ivar/gateway/config/WebClientConfig.java
+++ api-gateway/src/main/java/com/ivar/gateway/config/WebClientConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
-    @value("${auth.service.url}")
+    @Value("${auth.service.url}")
     private String authServiceUrl;
     
     @Bean
